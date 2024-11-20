1647ac976d6e9c497b9d3be33587b4e5b9363e0b
Danjim238 <158765030+Danjim238@users.noreply.github.com>
2024-11-20 12:26:42 +0200

    Update installationscript.sh

--- coit-frontend/script/installationscript.sh
+++ coit-frontend/script/installationscript.sh
@@ -3,9 +3,9 @@
 sudo apt-get update
 sudo apt-get install nodejs npm -y
 
-#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-#unzip awscliv2.zip
-#sudo ./aws/install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 
-#aws s3 cp s3://codebuild-practice-danjim238/build/ /home/ubuntu/build/ --recursive
+aws s3 cp s3://codebuild-practice-danjim238/build/ /home/ubuntu/build/ --recursive
